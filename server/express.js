066795d2b08e90e02f538b4d39dc3cd64bbbdabf
viza <vzaytsev@my.centennialcollege.ca>
2024-11-10 12:15:07 -0500

    reformat and add log for error

--- server/express.js
+++ server/express.js
@@ -29,6 +29,7 @@ app.use(cors());
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
+    console.log(err);
   } else if (err) {
     res.status(400).json({ error: err.name + ": " + err.message });
     console.log(err);
